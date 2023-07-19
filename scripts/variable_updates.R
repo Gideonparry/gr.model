1eba8358b9fae5284b0eda8c6186f19c9773484f
Gideonparry <gideonparry@protonmail.com>
2023-07-19 11:50:55 -0600

    script update

--- scripts/variable_updates.R
+++ scripts/variable_updates.R
@@ -1,16 +1,16 @@
-library(stringr)
-all_codes <- list.dirs("C:\Users\bean_student\Documents\meta_compatable\codes\moved_data",
-                       recursive = FALSE)
-all_codes <- str_sub(all_codes,-6,-1)
+path <- "C:\\Users\\bean_student\\Documents\\meta_compatable\\codes\\moved_data"
+all_codes <- list.dirs(path, recursive = FALSE)
+all_codes <- sub(".*moved_data/", "", all_codes)
 all_codes
 data_list <- list()
 
 for(i in seq_along(all_codes)){
-  data_list[[i]] <- code_var(path, all_codes[i])
+  data_list[[i]] <- code_var(path, all_codes[i], 4)
 }
+length(data_list)
 data_list[1]
 
-
+data_list[124]
 ######################## saving them ##########################################
 for (i in data_list){
   N <- names(i)
