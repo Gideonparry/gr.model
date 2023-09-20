b69d0e3e48f5104e222c4e7c13977d2594839c84
Gideonparry <gideonparry@protonmail.com>
2023-09-20 15:12:50 -0600

    done more models

--- cross_validation.R
+++ cross_validation.R
@@ -1,8 +1,108 @@
 data <- read.csv("C:\\Users\\bean_student\\Documents\\gr_model_data.csv")
-acc_test(data = data, seed = 1234)
+acc_test(data = data, seed = 123)[1:4]
+acc_test(data = data, seed = 123)[5:8]
+acc_test(data = data, seed = 123)[9:12]
+acc_test(data = data, seed = 123)[13:16]
 
 
 
+acc_test(data = data, seed = 1234)[1:4]
+acc_test(data = data, seed = 1234)[5:8]
+acc_test(data = data, seed = 1234)[9:12]
+acc_test(data = data, seed = 1234)[13:16]
 
 
 
+acc_test(data = data, seed = 2319)[1:4]
+acc_test(data = data, seed = 2319)[5:8]
+acc_test(data = data, seed = 2319)[9:12]
+acc_test(data = data, seed = 2319)[13:16]
+
+
+## Getting just new results
+acc_test(data = data, seed = 123,
+                  formula2 = "sqrtgr ~ logground + roofflat*Exposure +
+                  roofflat*winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 1234,
+         formula2 = "sqrtgr ~ logground + roofflat*Exposure +
+                  roofflat*winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 2319,
+         formula2 = "sqrtgr ~ logground + roofflat*Exposure +
+                  roofflat*winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+
+## No building info
+acc_test(data = data, seed = 123,
+         formula2 = "sqrtgr ~ logground +
+                  winter_wind +
+                   temp_avg")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 1234,
+         formula2 = "sqrtgr ~ logground +
+                  winter_wind +
+                   temp_avg")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 2319,
+         formula2 = "sqrtgr ~ logground +
+                  winter_wind +
+                   temp_avg")[c(3,7,11,15)]
+
+
+## drop heated/flat
+acc_test(data = data, seed = 123,
+         formula2 = "sqrtgr ~ logground + Exposure +
+                  winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 1234,
+         formula2 = "sqrtgr ~ logground + Exposure +
+                  winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 2319,
+         formula2 = "sqrtgr ~ logground + Exposure +
+                  winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+## keep flat drop interaction
+acc_test(data = data, seed = 123,
+         formula2 = "sqrtgr ~ logground + roofflat + Exposure +
+                  winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 1234,
+         formula2 = "sqrtgr ~ logground + roofflat + Exposure +
+                  winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 2319,
+         formula2 = "sqrtgr ~ logground + roofflat + Exposure +
+                  winter_wind +
+                  log(Size) + temp_avg + Parapet")[c(3,7,11,15)]
+
+### Exposure, flat and winter wind only
+acc_test(data = data, seed = 123,
+         formula2 = "sqrtgr ~ logground + roofflat*Exposure +
+                  roofflat*winter_wind")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 1234,
+         formula2 = "sqrtgr ~ logground + roofflat*Exposure +
+                  roofflat*winter_wind")[c(3,7,11,15)]
+
+acc_test(data = data, seed = 2319,
+         formula2 = "sqrtgr ~ logground + roofflat*Exposure +
+                  roofflat*winter_wind")[c(3,7,11,15)]
+
+colnames(data)
+
+### rf with weather, exposure and flat
+acc_test(data, 123, tree_vars = c(7,9,13,24,31,22))[c(4,8,12,16)]
+
+acc_test(data, 1234, tree_vars = c(7,9,13,24,31,22))[c(4,8,12,16)]
+
+acc_test(data, 2319, tree_vars = c(7,9,13,24,31,22))[c(4,8,12,16)]
